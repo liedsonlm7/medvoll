c6683d0154c711786a90e790b5f6205e13db530e
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-08 11:49:58 -0300

    create entities

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,4 +1,21 @@
 package med.voll.api.endereco;
 
-public record Endereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
 }
