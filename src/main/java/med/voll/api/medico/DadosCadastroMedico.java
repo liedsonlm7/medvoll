5e9a267262d87fed61bf25b55dbb56733496ab8c
liedsonlm7 <liedsonlm7@gmail.com>
2025-10-08 12:06:21 -0300

    create repository

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
+import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.Endereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
